360b1a2483933c7a8f05cc0f26f3b6fde3edbb6f
Gustavo Zimermann <gustavo.zimermann@catolicasc.edu.br>
2025-08-27 19:36:04 -0300

    Adicionando atividade 4

--- Programação/Atividade_4.cpp
+++ Programação/Atividade_4.cpp
@@ -2,11 +2,12 @@
 #include<stdlib.h>
 
 int main (void){
-	printf("Digite a Temperatura em Celsius: ");
 	float temperaturaCelsius;
+	printf("Digite a Temperatura em Celsius: ");
 	scanf("%f",&temperaturaCelsius);
+	fflush(stdin);
 	float temperaturaFahrenheit = (temperaturaCelsius*9/5)+32;
 	printf("Temperatura Fahrenheit: %.2f",temperaturaFahrenheit);
-	
+	fflush(stdin);
 	return 0;
 }
